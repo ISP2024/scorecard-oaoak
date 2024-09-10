d2e47fcde00e63627645f0a98cc3260182a882a2
Khongnat Baibua <92802658+oaoak@users.noreply.github.com>
2024-09-10 15:04:53 +0700

    add typehints as in instruction and fix some warnings.

--- scorecard.py
+++ scorecard.py
@@ -24,34 +24,34 @@ class Scorecard:
 
     def __init__(self):
         """Iniiialize a new Scorecard."""
-        self.scores = []
+        self.scores: list[float] = []
 
-    def add_score(self, score):
+    def add_score(self, score: float):
         """Add a score to the Scorecard."""
         self.scores.append(score)
 
-    def average(self):
+    def average(self) -> float:
         """Return the average of all scores, 0 if no scores."""
         return sum(self.scores)/max(1,len(self.scores))
 
 
-def print_scores(score_card):
+def print_scores(score_card: Scorecard):
     """Print statistics for the scorecard and the actual scores."""
 
     # What changes to Scorecard are needed in order to make this code work?
-    print(f"Scorecard contains {len(score_card)} scores.")
-    print(f"Min score: {min(score_card)}  Max score: {max(score_card)}.")
+    print(f"Scorecard contains {len(score_card.scores)} scores.")
+    print(f"Min score: {min(score_card.scores)}  Max score: {max(score_card.scores)}.")
     # What change to Scorecard is needed to make this work?
-    for score in score_card:
+    for score in score_card.scores:
         print(score)
 
 
-def ordinal(num):
+def ordinal(num) -> str:
     """Return the ordinal value of an integer; works for numbers up to 20.
 
     For examples: ordinal(1) is '1st', ordinal(2) is '2nd'.
     """
-    suffixes = {1: "st", 2: "nd", 3: "rd"}
+    suffixes: dict[int, str] = {1: "st", 2: "nd", 3: "rd"}
     return str(num) + suffixes.get(num, "th")
 
 
@@ -62,8 +62,8 @@ if __name__ == "__main__":
     print("Input 3 scores.")
     for count in range(1,4):
         score = input(f"input {ordinal(count)} score: ")
-        scorecard.add_score(score)
+        scorecard.add_score(float(score))
 
-    print("The average is " + scorecard.average())
+    print("The average is ", scorecard.average())
 
     print_scores(scorecard)
